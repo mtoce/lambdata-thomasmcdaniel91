5bf57aeb6f343ef5763b9591068c9a92d74d317e
ThomasMcDaniel91 <thomasm9105@gmail.com>
2020-05-05 14:48:37 -0500

    fixed some import errors

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lambdata-thomasmcdaniel91", # Replace with your own username
+    version="0.0.1",
+    author="Thomas McDaniel",
+    author_email="thomasm9105@gmail.com",
+    description="helper functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThomasMcDaniel91/lambdata-thomasmcdaniel91",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
